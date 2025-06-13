41ebf8938204e1d43b493afb306114007d4ce45f
redhat123456 <1907065810@qq.com>
2025-06-13 19:27:49 +0800

    ss

---
+++ main.py
@@ -0,0 +1,60 @@
+import yt_dlp
+import json
+from datetime import datetime, timedelta
+from jinja2 import Template
+import os
+import requests
+
+CHANNEL_URL = 'https://www.youtube.com/@NBCNewsKids'
+
+
+def fetch_videos():
+    ydl_opts = {'extract_flat': True, 'quiet': True, 'forcejson': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(CHANNEL_URL, download=False)
+        return info.get('entries', [])
+
+
+def get_video_stats(video_id):
+    with yt_dlp.YoutubeDL({'quiet': True, 'forcejson': True}) as ydl:
+        info = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
+        return {
+            'views': info.get('view_count', 0),
+            'title': info.get('title', ''),
+            'url': info.get('webpage_url', ''),
+            'thumbnail': info.get('thumbnail', '')
+        }
+
+
+def pick_top_recent(videos):
+    one_week = datetime.utcnow() - timedelta(days=7)
+    candidates = []
+    for vid in videos:
+        if 'upload_date' not in vid:
+            continue
+        ud = datetime.strptime(vid['upload_date'], '%Y%m%d')
+        if ud > one_week:
+            stats = get_video_stats(vid['id'])
+            candidates.append(stats)
+    return max(candidates, key=lambda x: x['views']) if candidates else None
+
+
+def update_webpage(video):
+    with open('templates/index_template.html') as f:
+        tpl = Template(f.read())
+    html = tpl.render(title=video['title'], url=video['url'], date=datetime.utcnow().strftime('%Y-%m-%d'))
+    os.makedirs('output', exist_ok=True)
+    with open('output/index.html', 'w') as f:
+        f.write(html)
+    # download thumbnail
+    if video.get('thumbnail'):
+        thumb = requests.get(video['thumbnail'])
+        with open('output/thumb.jpg', 'wb') as f:
+            f.write(thumb.content)
+
+
+if __name__ == '__main__':
+    vids = fetch_videos()
+    top = pick_top_recent(vids)
+    if top:
+        update_webpage(top)
